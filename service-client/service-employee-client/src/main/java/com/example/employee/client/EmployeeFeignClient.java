c97576539ea08ee5f20c3cac3edf9df3952716d4
SheZhiWei <1113075611@qq.com>
2023-07-06 11:58:09 +0800

    import openfeign

--- service-client/service-employee-client/src/main/java/com/example/employee/client/EmployeeFeignClient.java
+++ service-client/service-employee-client/src/main/java/com/example/employee/client/EmployeeFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("service-employee")
+@FeignClient(url = "localhost:8081", name = "service-employee")
 public interface EmployeeFeignClient {
     @GetMapping("/employee/inner/selectById/{id}")
     Employee selectById(@PathVariable Long id);
